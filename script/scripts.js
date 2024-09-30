fd1b69ceed76d574bd8118386b616c772807e337
MAC <mac@KashParas-Macbook-Pro-13.local>
2024-09-30 08:07:15 +0100

    adding

---
+++ script/scripts.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $(".icon").hover(function(){
+        $("span").animate({right: '50px'});
+    }, function(){
+        $("span").animate({right: '0px'});
+    });
+    
+   
+});
+
